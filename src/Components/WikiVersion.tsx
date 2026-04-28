b897836391aa2f41d8ad34eb3a257e491e3a5744
gabor-smith <g20kovacs01@proton.me>
2026-04-29 00:09:46 +0200

    Wiki version display

---
+++ src/Components/WikiVersion.tsx
@@ -0,0 +1,48 @@
+import { CircleAlert, CircleCheck } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
+enum VersionStatus {
+    Unknown,
+    OK,
+    Outdated
+}
+
+type WikiVersionProps = {
+    releaseVersion?: string;
+    wikiVersion?: string;
+}
+
+export default function WikiVersion({ releaseVersion, wikiVersion }: WikiVersionProps) {
+
+    let status: VersionStatus = VersionStatus.Unknown;
+    let versionNotice: string = "Wiki version is unknown!";
+    let versionColor: string = "text-red-500";
+
+    if(!(releaseVersion === null && wikiVersion === null)) {
+        if(releaseVersion === wikiVersion) {
+            versionNotice = "Wiki version is up to date";
+            status = VersionStatus.OK;
+            versionColor = "text-green-500";
+        } else {
+            versionNotice = "Does not match release version!";
+            status = VersionStatus.Outdated;
+            versionColor = "text-orange-500";
+        }
+    }
+
+    return (
+        <Tooltip>
+            <TooltipTrigger>
+                <p className={`inline-block relative ${versionColor} mr-4`}>
+                    {wikiVersion ?? "Error"}
+                    {status === VersionStatus.OK ? 
+                    <CircleCheck size="16" className="absolute top-[-10px] right-[-18px]" /> :
+                    <CircleAlert size="16" className="absolute top-[-10px] right-[-18px]" />}
+                </p>
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>{versionNotice}</p>
+            </TooltipContent>
+        </Tooltip>
+    )
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -12,9 +12,7 @@ import {
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
@@ -22,78 +20,23 @@ import {
 import { SelectScrollable, type SelectItems } from "@/Components/SelectScrollable";
 import { List  } from "lucide-react";
 import { InfoFreshIcon } from "@/Components/InfoFreshIcon";
-
-// <Button size="lg">
-//     <Link to="/releases">Releases</Link>
-// </Button>
-
-const northAmerica = [
-    { label: "Eastern Standard Time", value: "est" },
-    { label: "Central Standard Time", value: "cst" },
-    { label: "Mountain Standard Time", value: "mst" },
-    { label: "Pacific Standard Time", value: "pst" },
-    { label: "Alaska Standard Time", value: "akst" },
-    { label: "Hawaii Standard Time", value: "hst" },
-]
-
-const europeAfrica = [
-    { label: "Greenwich Mean Time", value: "gmt" },
-    { label: "Central European Time", value: "cet" },
-    { label: "Eastern European Time", value: "eet" },
-    { label: "Western European Summer Time", value: "west" },
-    { label: "Central Africa Time", value: "cat" },
-    { label: "East Africa Time", value: "eat" },
-]
-
-const asia = [
-    { label: "Moscow Time", value: "msk" },
-    { label: "India Standard Time", value: "ist" },
-    { label: "China Standard Time", value: "cst_china" },
-    { label: "Japan Standard Time", value: "jst" },
-    { label: "Korea Standard Time", value: "kst" },
-    { label: "Indonesia Central Standard Time", value: "ist_indonesia" },
-]
-
-const australiaPacific = [
-    { label: "Australian Western Standard Time", value: "awst" },
-    { label: "Australian Central Standard Time", value: "acst" },
-    { label: "Australian Eastern Standard Time", value: "aest" },
-    { label: "New Zealand Standard Time", value: "nzst" },
-    { label: "Fiji Time", value: "fjt" },
-]
-
-const southAmerica = [
-    { label: "Argentina Time", value: "art" },
-    { label: "Bolivia Time", value: "bot" },
-    { label: "Brasilia Time", value: "brt" },
-    { label: "Chile Standard Time", value: "clt" },
-]
-
-const items: SelectItems[] = [
-    { label: "Select a timezone", value: null },
-    ...northAmerica,
-    ...europeAfrica,
-    ...asia,
-    ...australiaPacific,
-    ...southAmerica,
-]
+import WikiVersion from "@/Components/WikiVersion";
 
 export default function Home() {
     return (
-        
             <Table>
                 <TableHeader>
                 <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Release</TableHead>
-                    <TableHead>Wiki</TableHead>
-                    <TableHead>Release Candidates</TableHead>
-                    <TableHead>Tags</TableHead>
-                    <TableHead>Features</TableHead>
-                    <TableHead>
+                    <TableHead className="text-center">Name</TableHead>
+                    <TableHead className="text-center">Release</TableHead>
+                    <TableHead className="text-center">Wiki</TableHead>
+                    <TableHead className="text-center">Release Candidates</TableHead>
+                    <TableHead className="text-center">Tags</TableHead>
+                    <TableHead className="text-center">Features</TableHead>
+                    <TableHead className="text-center">
                         <Button size="lg">
-                            <List />
                             <Link to="/releases">Releases</Link>
+                            <List data-icon="inline-end" />
                         </Button>
                     </TableHead>
                 </TableRow>
@@ -101,9 +44,13 @@ export default function Home() {
                 <TableBody>
                 {AppList.map((app, index) => (
                     <TableRow key={index}>
-                        <TableCell>{app.name}</TableCell>
-                        <TableCell>{app.version}</TableCell>
-                        <TableCell>{app.wikiVersion}</TableCell>
+                        <TableCell className="text-center">{app.name}</TableCell>
+                        <TableCell className="text-center">{app.version ?? "-"}</TableCell>
+                        <TableCell>
+                            <div className="flex flex-col items-center">
+                                <WikiVersion releaseVersion={app.version} wikiVersion={app.wikiVersion} />
+                            </div>
+                        </TableCell>
                         <TableCell>
                             <SelectScrollable
                                 items={app.releaseCandidates.map(rc => ({ label: rc, value: rc }))}
@@ -124,10 +71,11 @@ export default function Home() {
                                 }}
                             />
                         </TableCell>
-                        <TableCell>{app.features.map((feature, idx) => <div key={idx}>{feature.branch}</div>)}</TableCell>
+                        <TableCell>Open features</TableCell>
                         <TableCell>
                             <InfoFreshIcon timestamp={app.updated} />
                         </TableCell>
+                        <TableCell>compat</TableCell>
                     </TableRow>
                 ))}
                 </TableBody>
