f49951894b8bad1aeb1d75ec37df15e90b43110d
gabor-smith <g20kovacs01@proton.me>
2026-05-06 23:37:36 +0200

    Feature branch accordions with pagination

--- src/Components/FeatureList.tsx
+++ src/Components/FeatureList.tsx
@@ -2,7 +2,6 @@ import {
     Dialog,
     DialogClose,
     DialogContent,
-    DialogDescription,
     DialogFooter,
     DialogHeader,
     DialogTitle,
@@ -12,6 +11,23 @@ import { Button } from "@/Components/ui/button";
 import { PanelTopOpen, CircleAlert } from "lucide-react";
 import type { Feature } from "../Types/Application";
 import { parseElapsed, dateSort } from "@/Helper/Parsers";
+import { useState } from "react";
+import Paginator from "@/Components/ui/paginator";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/Components/ui/accordion"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/Components/ui/card"
+
+const PAGINATE_SIZE = 5;
 
 export default function FeatureList({ features }: { features: Feature[] | null }) {
 
@@ -26,39 +42,52 @@ export default function FeatureList({ features }: { features: Feature[] | null }
         }
     }
 
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const totalPages = Math.ceil((features?.length || 0) / PAGINATE_SIZE);
+    const currentFeatures = features?.slice((currentPage - 1) * PAGINATE_SIZE, currentPage * PAGINATE_SIZE);
+
     return (
         features?.length ?
-            <Dialog>
-                <DialogTrigger render={
-                    <Button className="w-full" variant="outline">
-                        <div className="flex flex-row items-center w-full">
-                        <CircleAlert size="16" className="text-orange-500 mr-1"></CircleAlert>
-                        <span className="flex flex-grow-1 ml-1">{lastCommitedFeature.branch} - {lastCommitAge} ago</span>
-                        <PanelTopOpen data-icon="inline-end" />
-                        </div>
-                    </Button>
-                } />
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Feature branches</DialogTitle>
-                        <DialogDescription>
-                            This dialog has a sticky footer that stays visible while the content
-                            scrolls.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="-mx-4 no-scrollbar max-h-[50vh] overflow-y-auto px-4">
-                    {features.map((feature, index) => (
-                        <p key={index} className="mb-4 leading-normal">
-                            {feature.branch}
-                        </p>
-                    ))}
+        <Dialog>
+            <DialogTrigger render={
+                <Button className="w-full" variant="outline">
+                    <div className="flex flex-row items-center w-full">
+                    <CircleAlert size="16" className="text-orange-500 mr-1"></CircleAlert>
+                    <span className="flex flex-grow-1 ml-1">{lastCommitedFeature.branch} - {lastCommitAge} ago</span>
+                    <PanelTopOpen data-icon="inline-end" />
                     </div>
-                    <DialogFooter>
-                        <DialogClose render={<Button variant="outline">Close</Button>} />
-                    </DialogFooter>
-                </DialogContent>
-            </Dialog>
-            :
-            <span>-</span>
+                </Button>
+            } />
+            <DialogContent className="sm:max-w-2xl">
+                <DialogHeader>
+                    <DialogTitle>Feature branches</DialogTitle>
+                </DialogHeader>
+                <Card className="w-full py-0 border-0">
+                    <CardContent className="px-0 border-0">
+                        <Accordion>
+                        {currentFeatures?.map((feature, index) => (
+                            <AccordionItem key={feature.branch+index}>
+                                <AccordionTrigger>{feature.branch}</AccordionTrigger>
+                                <AccordionContent>
+                                    <span className="leading-normal">This is a feature branch ...</span>
+                                </AccordionContent>
+                            </AccordionItem>
+                        ))}
+                        </Accordion>
+                    </CardContent>
+                </Card>
+                <DialogFooter>
+                    <Paginator
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        onPageChange={(pageNumber) => setCurrentPage(pageNumber)}
+                        showPreviousNext />
+                    <DialogClose render={<Button variant="outline">Close</Button>} />
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+        :
+        <span>-</span>
     )
 }
