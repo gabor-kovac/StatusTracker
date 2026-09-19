1b50477f001e431361f6bc535935b12f71dbed04
gabor-smith <g20kovacs01@proton.me>
2026-09-19 19:32:22 +0200

    Remove unused scripts

--- json-joiner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd src/Assets/Repos
-if [ -z "$(ls)" ]; then
-    echo "[]" > ../applicationDataFile.json
-else
-    jq -s "." $(ls) > ../applicationDataFile.json
-fi
--- pre-build.sh
+++ pre-build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Launches pre build scripts
 
-echo "Starting JSON joiner"
-bash ./json-joiner.sh
-
 echo "Starting AppList generator"
 bash ./generate-application-data.sh > src/Assets/ApplicationData.ts && echo "Generated ApplicationData.ts"
